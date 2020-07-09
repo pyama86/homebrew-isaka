2ab4218bac68da27b088903b455bfa0240f88118
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-09 16:27:45 +0900

    Brew formula update for isaka version v0.0.4

--- isaka.rb
+++ isaka.rb
@@ -2,16 +2,16 @@
 class Isaka < Formula
   desc "tail log for Apache Kafka"
   homepage "https://github.com/pyama86/isaka"
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pyama86/isaka/releases/download/v0.0.3/isaka_0.0.3_darwin_amd64.tar.gz"
-    sha256 "12938945d2787206a0890adcbe9ec135d4cd4a5d29fb3b043ea9e14d69b08ef2"
+    url "https://github.com/pyama86/isaka/releases/download/v0.0.4/isaka_0.0.4_darwin_amd64.tar.gz"
+    sha256 "783f568b2fa93f7c0fae5b68f551bd304de9999f3855c273fd3037d360e2754e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/pyama86/isaka/releases/download/v0.0.3/isaka_0.0.3_linux_amd64.tar.gz"
-      sha256 "543ca198308ab4f96464db14a6c505ab803ae8e70a99417ecfe426679fccac91"
+      url "https://github.com/pyama86/isaka/releases/download/v0.0.4/isaka_0.0.4_linux_amd64.tar.gz"
+      sha256 "274743b3e3f0a59d49a6bb8a6ea2c6267b0fcae817294228bf7b89a700fa07a6"
     end
   end
 
